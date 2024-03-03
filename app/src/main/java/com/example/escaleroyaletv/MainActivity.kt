93bd77713b6258ffc7037ba4ea467d3d90b8f457
roefwin2 <regis-971@hotmail.com>
2024-03-03 22:54:38 +0100

    create splash screen

--- app/src/main/java/com/example/escaleroyaletv/MainActivity.kt
+++ app/src/main/java/com/example/escaleroyaletv/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.escaleroyaletv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.escaleroyaletv.presenter.EscaleRoyaleAppSBar
 import com.example.escaleroyaletv.presenter.VideoPlayerScreen
 import com.example.escaleroyaletv.presenter.models.EscaleRoyaleApp
 import com.example.escaleroyaletv.ui.theme.EscaleRoyaleTVTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.isLoading.value
+            }
+        }
         // Set the video path from the "res/raw" directory
         val videoPath = "android.resource://" + packageName + "/" + R.raw.escale_royale_video
 
